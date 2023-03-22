38e5f811fade8a986e3b673fa5dab088aa2f2641
HuyLucas <117262719+HuyLucas@users.noreply.github.com>
2023-03-22 11:15:02 +0700

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
